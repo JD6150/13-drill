05213b7e54edc5475ac1068324482f0660ac1a60
John Dunn <jd6150@gmail.com>
2015-08-25 11:27:30 -0700

    This is the real good one

---
+++ This works too/This works too/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This_works_too
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This works too");
+        }
+    }
+}
